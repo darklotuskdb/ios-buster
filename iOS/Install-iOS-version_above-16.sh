6bddf425645f6ef78bc729cfe96ea1ae59066fda
Kamaldeep Bhati <th.kamalsinghrajput@gmail.com>
2024-03-30 21:13:10 +0530

    Delete iOS directory

--- iOS/Install-iOS-version_above-16.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-apt-get update --fix-missing
-apt-get install -y gawk nano unzip coreutils tree libplist-utils cycript lldb lldb-14 liblldb-14 --allow-unauthenticated
--- iOS/Install-iOS-version_below-15.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-apt-get update --fix-missing
-apt-get install -y gawk nano unzip coreutils tree libplist-utils cycript lldb lldb-10 liblldb-10
